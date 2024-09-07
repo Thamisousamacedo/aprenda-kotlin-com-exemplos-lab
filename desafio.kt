1675e263b125bf0dc4bd4ed778e6d224457e2332
Thamirys Sousa Macêdo <154887343+Thamisousamacedo@users.noreply.github.com>
2024-09-07 15:56:40 -0300

    Alterado com comentários

--- desafio.kt
+++ desafio.kt
@@ -7,7 +7,8 @@ data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nive
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
    
    val inscritos = mutableListOf<Usuario>()
-   
+
+   //verificar se o usuário está matriculado usando o if e else, e iprimir a mensagem que estão matriculados
    fun matricular(usuario: Usuario) {
        if (!inscritos.contains(usuario)){
            inscritos.add(usuario)
@@ -17,6 +18,8 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
            println("${usuario.nome} já está matriculado(a) nessa formação") 
        }
    }
+
+   //imprimir o nome da formação, o conteúdo e os inscritos
    fun exibirDetalhes() {
        println("Formação: $nome")
        println("Conteúdos:")
@@ -24,11 +27,13 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
            println("-${conteudo.nome}(${conteudo.duracao}minutos, Nível: ${conteudo.nivel})")        
    }
    
+//imprimir o total de inscritos e puxar a quantidade de inscritos
    println("Total de inscritos: ${inscritos.size}")
    inscritos.forEach{println("-${it.nome}") }
    }
 }
 
+//aqui criei os conteúdos dos cursos, duração e nível
 fun main() {
     val kotlinBasico = ConteudoEducacional("Introdução ao Kotlin", 90, Nivel.BASICO)
     val kotlinIntermediario = ConteudoEducacional("Programação Funcional com Kotlin",120, Nivel.INTERMEDIARIO)
@@ -36,7 +41,7 @@ fun main() {
     
     val formacaoKotlin = Formacao("Formação Kotlin Developer", listOf(kotlinBasico, kotlinIntermediario, kotlinAvancado))
     
-    
+//nessa parte foram nomeados os usuários  
     val usuario1 = Usuario("Thami")
     val usuario2 = Usuario("Guilherme")
     
